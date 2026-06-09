426dd558bfaf8f10a586237aa8e118da1adbe2c2
IamAlfonceMN <alfoncemuthama04@gmail.com>
2026-06-09 16:51:34 +0300

    i added a new file in my repository

---
+++ prepend.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedLists:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+          print(temp.value)
+          temp = temp.next
+
+
+    def append (self, value):
+         new_node = Node (value)
+         if self.head == 0:
+             self.head = new_node
+             self.tail = new_node
+         else:
+             self.tail.next = new_node
+             self.tail = new_node
+         self.length +=1
+
+
+    def prepend (self, value):
+           new_node = Node(value)
+           if self.length==0:
+              self.head = new_node
+              self.tail = new_node
+
+           else:
+              new_node.next = self.head
+              self.head = new_node
+
+              self.length+=1
+              return True
+
+
+my_linked_list = LinkedLists(2)
+my_linked_list.append(2)
+
+my_linked_list.prepend(7)
+
+print(my_linked_list.print_list())
+print(my_linked_list.head.value)
+print(my_linked_list.head.next.value)
+
